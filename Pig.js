0b4f0158a63edc065b410579d1af70871749a693
shreeparnaroy <73882736+shreeparnaroy@users.noreply.github.com>
2020-12-29 20:59:42 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,57 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+
+    this.deeevivibilty=255
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+display(){
+console.log(this.body.speed)
+if(this.body.speed<2.5){
+  super.display()
+
+
+
+
+
+}
+//project call neeti mam 
+else{
+    push                                                              ()
+   
+    World                               .remove(world,this.body)
+this.deeevivibilty-=1
+tint                                      (255,this.deeevivibilty)
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+
+pop()
+
+}
+}
+
+
+
+
+};
